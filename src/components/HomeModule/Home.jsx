284203efba2b090935c521a23ea3f94b7ecf1170
priyamgupta-1999 <priyam.guptaofficial@gmail.com>
2026-01-10 21:41:19 +0530

    cod placement change

--- src/components/HomeModule/Home.jsx
+++ src/components/HomeModule/Home.jsx
@@ -190,12 +190,12 @@ const Home = () => {
 
       <h2 className='heading'>{t('Top Factories')}</h2>
       <FactoryListing loading={factoriesLoading} data={factoriesData?.data} />
+      <img src={'https://res.cloudinary.com/dy6k69ynu/image/upload/v1768060518/FootwearMandi_Presentation_8_ewxfar.png'} style={{width:'100%',aspectRatio:'16/9',marginBottom:'2em',marginTop:'1em'}} />
 
       <h2 className='heading'>{t('Top Products')}</h2>
 
       <ProductListing1 loading={isLoading} data={data?.data?.PrimaryData} />
 
-      <img src={'https://res.cloudinary.com/dy6k69ynu/image/upload/v1768060518/FootwearMandi_Presentation_8_ewxfar.png'} style={{width:'100%',aspectRatio:'16/9',marginBottom:'2em',marginTop:'1em'}} />
       <Cards1 data={NavigationLinksCards1} />
 
       {/* <ProductListing2 loading={isLoading} data={data?.data?.SmallSlider}/> */}
